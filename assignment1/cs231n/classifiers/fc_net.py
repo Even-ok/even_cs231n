9208cc056fd8544b89b1fe048395491a79768145
Even-ok <1284915396@qq.com>
2023-02-01 19:31:30 +0800

    20230201part2

--- assignment1/cs231n/classifiers/fc_net.py
+++ assignment1/cs231n/classifiers/fc_net.py
@@ -2,9 +2,12 @@ from builtins import range
 from builtins import object
 import numpy as np
 
-from ..layers import *
-from ..layer_utils import *
-
+# from ..layers import *
+# from ..layer_utils import *
+import sys
+sys.path.append('/home/PJLAB/liangyiwen/Even/techLearning/cs231n/assignment1/cs231n/')
+from layers import *
+from layer_utils import *
 
 class TwoLayerNet(object):
     """
@@ -55,7 +58,10 @@ class TwoLayerNet(object):
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        self.params['W1'] = weight_scale * np.random.randn(input_dim, hidden_dim)
+        self.params['b1'] = np.zeros(hidden_dim)
+        self.params['W2'] = weight_scale * np.random.randn(hidden_dim, num_classes)
+        self.params['b2'] = np.zeros(num_classes)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
@@ -88,7 +94,11 @@ class TwoLayerNet(object):
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        # aff1_output = X.dot(self.params['W1']) + self.params['b1']
+        aff1_relu_output, cache1 = affine_relu_forward(X, self.params['W1'], self.params['b1'])
+        aff2_output = affine_forward(aff1_relu_output, self.params['W2'], self.params['b2'])
+        scores = aff2_output
+
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
@@ -112,7 +122,15 @@ class TwoLayerNet(object):
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        loss, dout = softmax_loss(scores, y) 
+        loss += 0.5 * self.reg * (np.sum(self.params["W1"] ** 2) + \
+                                  np.sum(self.params["W2"] ** 2))
+
+        dx_hidden, grads["W2"], grads["b2"] = affine_backward(dout, (aff1_relu_output,self.params["W1"], self.params["W2"]))
+
+
+        # aff1_relu_back = relu_backward(dx_hidden, aff1_output)
+        # _, grads["W1"], grads["b1"] = affine_backward(aff1_relu_back, X)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
